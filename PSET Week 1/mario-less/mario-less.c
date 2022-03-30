a38f8af5f60a46e27f5e64baf8ffecc21fb3fcaf
CarolDill <carol37_dill@msn.com>
2022-03-29 23:57:28 -0700

    Week 1 exercises

---
+++ PSET Week 1/mario-less/mario-less.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int height;
+
+int main(void)
+{
+    do
+    {
+        //get input from user
+        height = get_int("Height: ");
+    }
+    //continue asking for inputs until you get a positive number between 1 and 8
+    while (height <= 0 || height > 8);
+
+    //to align blocks to the right
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = height - 1; j > i; j--)
+        {
+            printf(" ");
+        }
+        //to print blocks
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
